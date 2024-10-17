04bce9dada79e758029affc9e655fb7073d3bb0b
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-17 23:04:18 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -33,7 +33,7 @@ async (conn, mek, m, { from, q, reply }) => {
 💨 *Wind Speed*: ${data.wind.speed} m/s
 🔽 *Pressure*: ${data.main.pressure} hPa
 
-Powered by OpenWeatherMap 🌦️
+> Powered by DENETH-MD 🌦️
 `;
 
         return reply(weather);
