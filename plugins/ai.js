ca12adfc6087f313e16c936bfbc6cfe395fde478
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-17 22:39:49 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -14,7 +14,7 @@ cmd({
         let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=hi`);
 
         // Reply with the fetched data
-        return reply(from,{ image: { url: `https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true` }, caption: '${data:data}' });
+        await conn.sendMessage(from,{ image: { url: `https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true` }, caption: '${data:data}' });
     } catch (e) {
         console.log(e);
         reply(`${e}`)
