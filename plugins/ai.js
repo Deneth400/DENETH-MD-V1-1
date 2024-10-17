899a8168663802f062a00ffb8c8010f70993ca6f
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-17 11:12:30 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,21 +1,20 @@
-const config = require('../config');
-const { cmd, commands } = require('../command');
-const { fetchJson } = require('../lib/functions');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
 
 cmd({
     pattern: "ai",
-    desc: "AI chat",
+    desc: "ai",
     category: "main",
-    react: "🧠",
+    react: "👾",
     filename: __filename
-}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        // Fetch response from the AI API
-        let data = `https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`
-
-        // Reply with the fetched data
-        return await conn.sendMessage(from,{image: "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true"},caption: `${data.data}`},{quoted: mek})
-        console.log(e);
-        reply(e.toString());
-    }
-});
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+reply({image: "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true"},`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
