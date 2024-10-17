31933d8b4f9e2136882aa399bdb8dd3a0d32e70f
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-17 11:15:01 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,20 +1,19 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
+const { fethJson } = require('../lib/functions')
 
 cmd({
     pattern: "ai",
-    desc: "ai",
+    desc: "ai chat",
     category: "main",
-    react: "👾",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
-reply({image: "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true"},`${data.data}`)
+let data = await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}')
+return reply({image: "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true",}'${data.data}')
 }catch(e){
-console.log(e)
-reply(`${e}`)
+  console.log(e)
+  reply('${e}')
 }
-})
+}}
