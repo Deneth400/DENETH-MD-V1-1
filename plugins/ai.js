fcf7a78b6a7cdc78beeff1859393982f31b21e9a
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-17 12:40:37 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -11,10 +11,10 @@ cmd({
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Fetch response from the AI API
-        let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`);
+        let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q} n\> >​🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ ​🇩​​🇪​​🇳​​🇪​​🇹​​🇭​-​🇲​​🇩`​);
 
         // Reply with the fetched data
-        return await conn.sendMessage(from,{ image: { url: `https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true` }, caption: (data.data)n\> > ​🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ ​🇩​​🇪​​🇳​​🇪​​🇹​​🇭​-​🇲​​🇩​ });
+        return await conn.sendMessage(from,{ image: { url: `https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/AI.jpg?raw=true` }, caption: (data.data)​ });
     } catch (e) {
         console.log(e);
         reply(e.toString());
