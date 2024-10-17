fd2bf81a3a3bbb9ecf17d82206c8601238a1fde0
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-17 10:19:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "gmlEQQgK#I3p8_RkDchmZYAjNsuKWATOwQIJjyJW6C_ReK7-4oMY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/Alive.jpg?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*I am Alive Now*\n \n  A fast and responsive multi-device WhatsApp bot built using Baileys and various APIs. It offers seamless functionality without buttons, delivering quick and efficient performance for automated tasks and commands.\n\n > ​🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ ​🇩​​🇪​​🇳​​🇪​​🇹​​🇭​-​🇲​​🇩​",
+ALIVE_MSG: process.env.ALIVE_MSG || "*I am Alive Now*\n \n  A fast and responsive multi-device WhatsApp bot built using Baileys and various APIs. It offers seamless functionality without buttons, delivering quick and efficient performance for automated tasks and commands.\n\n> > ​🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ ​🇩​​🇪​​🇳​​🇪​​🇹​​🇭​-​🇲​​🇩​",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
