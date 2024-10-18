742a134bd90b9184ca5eb04e6824c1dfc536e2a8
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-18 23:29:21 +0530

    Update yt+song+video.js

--- plugins/yt+song+video.js
+++ plugins/yt+song+video.js
@@ -2,93 +2,100 @@ const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
 
-
-cmd({
-    pattern: "video",
-    desc: "DOWNLOAD VIDEO DL",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("Please Give ME URL or Text")
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
-
-let desc = `
-*DENETH-MD YOUTUBE VIDEO DOWNLOADER*
-
-*Title*: ${data.title}
-*Description*: ${data.description}
-*Time*: ${data.timestamp}
-*Ago*: ${data.ago}
-*Views*: ${data.views}
-
-> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇɴᴇᴛʜ-ᴍᴅ 
-`
-
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-//======================DOWNLOAD-VIDEO==========================
-
-
-let down = await fg.ytv(url)
-let downloadUrl = down.dl_url
-
-//======Video Message====================
-await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"DENETH-MD POWER"},{quoted:mek})
-  
-  
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
-//==================Song Downloader====================
 cmd({
     pattern: "song",
-    desc: "DOWNLOAD SONG DL",
-    category: "download",
+    react: "🎵",
+    desc: "downlod song",
+    category: "downlod",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please Give ME URL or Text")
+
+if(!q) return reply("❌Please give me url or titel")
 const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
+const deta = search.videos[0];
+const url = deta.url 
 
-let desc = `
-*DENETH-MD YOUTUBE SONG DOWNLOADER*
+let desc= `
+*•.¸♡ 💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍 AUDIO-DOWNLOADER🎶 ♡¸.•*
+|__________________________
+| 🎠title : ${deta.title}
+| 🎠description : ${deta.description}
+| 🎠time : ${deta.timestamp}
+| 🎠ago : ${deta.ago}
+| 🎠views : ${deta.views}
+|__________________________
 
-*Title*: ${data.title}
-*Description*: ${data.description}
-*Time*: ${data.timestamp}
-*Ago*: ${data.ago}
-*Views*: ${data.views}
+POWERED by *⚡ DARK NEON CYBERS 🤍*
 
-> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇɴᴇᴛʜ-ᴍᴅ 
 `
 
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-//======================DOWNLOAD-AUDIO/document==========================
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
 
+//downlod audio+ document
 
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//======Audio Message====================
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"POWERED BY DENETH-MD"},{quoted:mek})
+//send audio message 
+await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg",caption :"*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋɴᴇᴏɴᴄʏʙᴇʀꜱ*"},{quoted:mek})
+await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"audio/mpeg",fileName:deta.title + ".mp3" ,caption :"*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋɴᴇᴏɴᴄʏʙᴇʀꜱ*"},{quoted:mek})
+
   
-  
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
 
+//========video dl=======
 
+cmd({
+    pattern: "video",
+    react: "🎬",
+    desc: "downlod video",
+    category: "downlod",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
 
+if(!q) return reply("❌Please give me url or title")
+const search = await yts(q)
+const deta = search.videos[0];
+const url = deta.url 
+
+let desc= `
+*•.¸♡ 💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍 VIDEO-DOWNLOADER📽️ ♡¸.•*
+|__________________________
+| 🎠title : ${deta.title}
+| 🎠description : ${deta.description}
+| 🎠time : ${deta.timestamp}
+| 🎠ago : ${deta.ago}
+| 🎠views : ${deta.views}
+|__________________________
+
+POWERED by *⚡ DARK NEON CYBERS 🤍*
+
+`
+
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
+
+//downlod video + document 
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video  message 
+await conn.sendMessage(from,{video:{url:downloadUrl},mimetype:"video/mp4",caption :"*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋɴᴇᴏɴᴄʏʙᴇʀꜱ*"},{quoted:mek})
+await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"video/mp4",fileName:deta.title + ".mp4",caption :"*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋɴᴇᴏɴᴄʏʙᴇʀꜱ*"},{quoted:mek})
+
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
