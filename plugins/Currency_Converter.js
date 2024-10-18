6cef9dd3bdd2c36b85ed3c6e6466818556ce4c9b
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-19 05:10:46 +0530

    Update Currency_Converter.js

--- plugins/Currency_Converter.js
+++ plugins/Currency_Converter.js
@@ -5,7 +5,7 @@ const { cmd, commands } = require('../command');
 cmd({
     pattern: "convert",
     desc: "Convert an amount from one currency to another.",
-    category: "utility",
+    category: "convert",
     react: "💱",
     filename: __filename
 },
