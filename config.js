e53896e07d508e041ffeb0563101b046d290669d
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-19 05:15:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "gmlEQQgK#I3p8_RkDchmZYAjNsuKWATOwQIJjyJW6C_ReK7-4oMY",
 MONGODB: process.env.MONGODB || "mongodb://mongo:cRkswBWkzOcbjKHaQfDsLLraLympnLQa@junction.proxy.rlwy.net:56096",
+PEXELS_API_KEY: process.env.PEXELS_API_KEY='39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH',
 };
