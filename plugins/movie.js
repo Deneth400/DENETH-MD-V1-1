27c02cf3ea94f5fbee15652fd00b2dbea428f04c
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-19 21:51:40 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -16,7 +16,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             return reply("📽️ Please provide the name of the movie.");
         }
 
-        const apiUrl = `https://www.omdbapi.com/?apikey=37033827`;
+        const apiUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=37033827&t=${match[1]}&plot=full}`;
         const response = await axios.get(apiUrl);
 
         const data = response.data;
